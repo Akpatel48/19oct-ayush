66331539dece8e1fee31500bd75d2fe32cea92be
Akpatel48 <148442756+Akpatel48@users.noreply.github.com>
2023-12-27 16:36:47 +0530

    Update 4(3).c

--- prectice/4(3).c
+++ prectice/4(3).c
@@ -1,30 +1,26 @@
 #include<stdio.h>
 int main(int argc, char const *argv[])
 {
-    int i,a[10],esum=0,osum=0;
+    int i,a,esum=0,osum=0,e=0,o=0;
+        
         for (i = 0; i < 10; i++)
         {
             printf("enter number");
-            scanf("%d",&a[i]);
-        }
-        for (i = 0; i < 10; i++)
-        {
-            if ((a[i]%2)==0)
+            scanf("%d",&a);
+            if ((a%2)==0)
             {
-                esum=a[i]+esum;
+                esum=a+esum;
+                e=e+1;
             }
             else
             {
-                osum=a[i]+osum;
+                osum=a+osum;
+                o=o+1;
             }
 
         }
         printf("even sum =%d",esum);
+        printf("even %d",e);
         printf("odd aum =%d",osum);
+        printf("even %d",o);
 }
-
-
-
-
-
-
