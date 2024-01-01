c8a25d7860dc35c8f04dd1f77a2484eb45f63079
Akpatel48 <148442756+Akpatel48@users.noreply.github.com>
2024-01-01 16:34:52 +0530

    Delete prectice directory

--- prectice/1.c
+++
@@ -1,8 +0,0 @@
-#include <stdio.h>
-int main()
-{
-    printf("name:-ayush\n");
-    printf("date of Birth date:-22/12/2004\n");
-    printf("age:-19\n");
-    printf("address:-thoriyali,post office vali seri,360110");
-}
