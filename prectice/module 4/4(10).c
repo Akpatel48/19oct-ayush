a0e851685baaeee11e5898281cc9805358f8e49a
Akpatel48 <148442756+Akpatel48@users.noreply.github.com>
2024-01-05 15:14:41 +0530

    4(10).c

--- prectice/module 4/4(10).c
+++ prectice/module 4/4(10).c
@@ -4,10 +4,13 @@ int main(int argc, char const *argv[])
     int a,b;
     printf("enter number");
     scanf("%d",&a);
-    b=a%10;
-    while (a>=10)
+    if(a>10)
     {
-        a=a/10;
+         b=a%10;
+        while (a>=10)
+        {
+            a=a/10;
+        }
     }
     a=a+b;
     printf("sum=%d",a);
