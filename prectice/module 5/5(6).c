1900f376887532bd67af5fe1676e454368eef08e
Akpatel48 <148442756+Akpatel48@users.noreply.github.com>
2024-01-16 16:41:07 +0530

    5(6).c

--- prectice/module 5/5(6).c
+++ prectice/module 5/5(6).c
@@ -3,30 +3,30 @@ int i,j,a=2,no[20][20],b[20][20],c,m1,m2,m3,m4;
 //char c;
 int arr()
 {
-
+    printf("enter first array\n");
     for (i = 0; i < a; i++)
     {
         for (j = 0; j < a; j++)
         {
-            printf("enter number");
+            
             scanf("%d",&no[i][j]);
 
         }
-
     }
+    printf("enter second array\n");
     for (i = 0; i < a; i++)
     {
         for (j = 0; j < a; j++)
         {
-            printf("enter number");
+           
             scanf("%d",&b[i][j]);
 
         }
-
     }
 }
 int arrpr()
 {
+    printf("first array");
     for (i = 0; i < a; i++)
     {
         for (j = 0; j < a; j++)
@@ -36,6 +36,7 @@ int arrpr()
         printf("\n");
     }
     printf("\n");
+    printf("second array");
     for (i = 0; i < a; i++)
     {
         for (j = 0; j < a; j++)
@@ -45,14 +46,10 @@ int arrpr()
         printf("\n");
     }
 }
-int m()
-{
-
-}
 int sw()
 {
-    printf("enter chose");
-    scanf("%D",&c);
+    printf("enter chose 1.add 2.sub 3.mul");
+    scanf("%d",&c);
     switch (c)
     {
         case 1:
@@ -77,9 +74,9 @@ int sw()
             break;
         case 3:
                 m1=no[0][0]*b[0][0]+no[0][1]*b[1][0];
-    m2=no[0][0]*b[0][1]+no[0][1]*b[1][1];
-    m3=no[1][0]*b[0][0]+no[1][1]*b[1][0];
-    m4=no[1][0]*b[0][1]+no[1][1]*b[1][1];
+                m2=no[0][0]*b[0][1]+no[0][1]*b[1][1];
+                m3=no[1][0]*b[0][0]+no[1][1]*b[1][0];
+                m4=no[1][0]*b[0][1]+no[1][1]*b[1][1];
             printf("%d",m1);
             printf(" %d\n",m2);
             printf("%d",m3);
@@ -91,6 +88,5 @@ int main(int argc, char const *argv[])
     arr();
     arrpr();
     sw();
-    m();
     return 0;
 }
