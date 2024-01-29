262b6484eba760cba6eb583078d6ba497a4bc906
Akpatel48 <148442756+Akpatel48@users.noreply.github.com>
2024-01-29 17:44:22 +0530

    Add files via upload

---
+++ prectice/c/module 4/4(2).c
@@ -0,0 +1,38 @@
+#include<stdio.h>
+int n1,n2,m;
+char s;
+void ev()
+{
+    printf("enter two values:-");
+    scanf("%d%d",&n1,&n2);
+    printf("chose +,-,*,/:-");
+    scanf("%s",&s);
+}
+void cal()
+{
+    switch (s)
+    {
+    case '+':
+        m=n1+n2;
+        break;
+    case '-':
+        m=n1-n2;
+        break;
+    case '*':
+        m=n1*n2;
+        break;
+    case '/':
+        m=n1/n2;
+    }
+}
+void pri()
+{
+    printf("%d%c%d=%d",n1,s,n2,m);
+}
+int main(int argc, char const *argv[])
+{
+    ev();
+    cal();
+    pri();
+    return 0;
+}
