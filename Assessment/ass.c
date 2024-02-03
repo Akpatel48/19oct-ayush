9319416a4c3a9a32d3af278d4f3584a59fe27625
Akpatel48 <148442756+Akpatel48@users.noreply.github.com>
2024-02-03 16:53:27 +0530

    ass.c

--- Assessment/ass.c
+++ Assessment/ass.c
@@ -1,58 +1,52 @@
 #include<stdio.h>
 int no1,no2,c;
-int cal()
+int enter()
 {
-    a:
     printf("---------------------menu---------------------");
-    printf("\n0.exit program\n1.Addition\n2.Substraction\n3.Multiplication\n4.Division\n\n");
+    printf("\n1.Addition\n2.Substraction\n3.Multiplication\n4.Division\n\n");
     printf("Enter your choice:");
     scanf("%d",&c);
-     while (c==0)
-    {
-        goto exti;
-    }
-    if (c<4)
-    {
-        printf("\nenter first number:");
-        scanf("%d",&no1);
-        printf("enter second number:");
-        scanf("%d",&no2);
-        printf("\n");
-    }
+}
+int cal()
+{
+    printf("\nenter first number:");
+    scanf("%d",&no1);
+    printf("enter second number:");
+    scanf("%d",&no2);
+    printf("\n");
     switch (c)
     {
-    case 1:
-        printf("Addition = %d",no1+no2);
-        printf("\n");
-        goto a;
-        break;
-    case 2:
-        printf("Substaction = %d",no1-no2);
-        printf("\n");
-        goto a;
-        break;
-    case 3:
-        printf("Multiplicattion = %d",no1*no2);
-        printf("\n");
-        goto a;
-        break;
-    case 4:
-        printf("Division = %d",no1/no2);
-        printf("\n");
-        goto a;
-        break;
-    default:
-            printf("\nInvalid choice!!!!");
+        case 1:
+            printf("Addition = %d",no1+no2);
             printf("\n");
-            goto a;
-        break;
+            break;
+        case 2:
+            printf("Substaction = %d",no1-no2);
+            printf("\n");
+            break;
+        case 3:
+            printf("Multiplicattion = %d",no1*no2);
+            printf("\n");
+            break;
+        case 4:
+            printf("Division = %d",no1/no2);
+            printf("\n");
+            break;
     }
-    exti:
-    printf("\n-----------------------------");
 }
 int main(int argc, char const *argv[])
 {
-
+    do
+    {
+        enter();
+            if(c==0 || c>=5)
+            {
+                printf("\nInvalid choice!!!!");
+                printf("\n-----------------------------");
+                goto exti;
+            }
         cal();
+    } while (c!=0);
+    exti:
     return 0;
 }
