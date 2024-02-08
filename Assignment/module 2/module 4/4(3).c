78cd8f91df6252b1c33660cb056de009cf16f9eb
Akpatel48 <148442756+Akpatel48@users.noreply.github.com>
2024-02-08 16:41:53 +0530

    4(3).c

--- Assignment/module 2/module 4/4(3).c
+++ Assignment/module 2/module 4/4(3).c
@@ -1,21 +1,24 @@
-#include<stdio.h>
-void rev(char s[],int i)
-{
-
-    if (s[i]='\0')
-    {
+#include <stdio.h>
+char temp;
+void revStr(char ch[], int s, int e) {
+    if (s >= e) {
         return;
-        rev(s,i+1);
-        printf("%c",s[i]);
     }
-
+    temp = ch[s];
+    ch[s] = ch[e];
+    ch[e] = temp;
+    revStr(ch, s + 1, e - 1);
 }
-int main(int argc, char const *argv[])
-{
-    char str[100];
+int main() {
+    char Str[100];
     printf("enter string:-");
-    gets(str);
-    printf("string reverse is ");
-    rev(str,0);
+    scanf("%s", Str);
+
+    int l = 0;
+    while (Str[l] != 0) {
+        l++;
+    }
+    revStr(Str, 0, l - 1);
+    printf("reverse string:-%s",Str);
     return 0;
 }
